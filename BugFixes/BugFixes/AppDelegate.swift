cd04b685dd37bb921516567458172977370ef141
Max Hooton <max.hooton@auden.co.uk>
2018-01-23 15:38:57 +0000

    desktop app

---
+++ BugFixes/BugFixes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BugFixes
+//
+//  Created by Max Hooton on 23/01/2018.
+//  Copyright © 2018 NordicArts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BugFixes/BugFixes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BugFixes
+//
+//  Created by Max Hooton on 23/01/2018.
+//  Copyright © 2018 NordicArts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
